63d47cc162e2de2e12da876f941aaa6955bae9b9
heshanisc <heshanisc2000@gmail.com>
2023-06-26 23:38:06 +0530

    Ratings modifications

--- src/components/Comments/Comment.js
+++ src/components/Comments/Comment.js
@@ -4,22 +4,22 @@ import Avatar from "../Shared/Avatar";
 const Comment = (props) => {
   return (
     <>
-      <div key={props.id} className="d-flex flex-row user p-2 mt-2 border-top">
-        <Avatar user={props.user} />
+      <div key={props?.id} className="d-flex flex-row user p-2 mt-2 border-top">
+        <Avatar user={props?.user} img={props?.img} />
         <div className="d-flex flex-column mx-2 my-auto">
           <span className="font-weight-bold" style={{ fontSize: "16px" }}>
-            {props.user}
+            {props?.user}
             <span style={{ fontSize: "12px", marginLeft: "8px" }}>
-              {props.role}
+              {props?.role}
             </span>
             <span style={{ fontSize: "12px", marginLeft: "8px" }}>
-              {props.time}
+              {props?.time}
             </span>
           </span>
         </div>
       </div>
       <div className="mt-2 p-2">
-        <p style={{ fontSize: "14px" }}>{props.message}</p>
+        <p style={{ fontSize: "14px" }}>{props?.message}</p>
       </div>
     </>
   );
